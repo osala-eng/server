9dc22a5798d8500185254338e0027e67911ac15e
osala-eng <64925863+osala-eng@users.noreply.github.com>
2021-12-08 06:18:46 +0300

    POST

--- dtbserver.js
+++ dtbserver.js
@@ -58,7 +58,7 @@ app.get("/",(req,res)=>{
     console.log("HTTP CLIENT CONNECTED")
 });
 
-app.get("/white",(req,res)=>{ 
+app.post("/white",(req,res)=>{ 
   res.sendStatus(200)
   console.log("White light trigger")
   MQTT_CLIENT.publish("Home/ctrl", 'A', { qos: 0, retain: false }, (error) => {
