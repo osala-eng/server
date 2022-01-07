a674f998c8dabc2619e4b52a403aece4f737858c
osala-eng <64925863+osala-eng@users.noreply.github.com>
2022-01-07 20:03:30 +0300

    Video server

--- dtbserver.js
+++ dtbserver.js
@@ -5,6 +5,7 @@ var md5 = require("md5")
 var server = require("http").createServer(app)
 var https = require("https")
 var fs = require("fs")
+const path = require('path')
 var WebSocket = require("ws")
 var mqtt = require("mqtt")
 
@@ -65,6 +66,46 @@ app.get("/",(req,res)=>{
     console.log("HTTP CLIENT CONNECTED")
 });
 
+app.get('/video', function(req, res) {
+  //const path = 'sample.mp4'
+  const path = 'worriornun.mkv'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    if(start >= fileSize) {
+      res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+      return
+    }
+    
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
 app.get("/white",(req,res)=>{ 
   res.sendStatus(200)
   console.log("White light trigger")
@@ -95,6 +136,13 @@ app.get("/fan",(req,res)=>{
       console.error(error)
     }
   })
+
+});
+
+app.get("/stream",(req,res)=>{ 
+  //res.sendStatus(200)
+  res.sendFile(path.join(__dirname + '/index.html'))
+  console.log("Video stream initiated")
 
 });
 
